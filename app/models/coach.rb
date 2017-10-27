5a406a84d6cbc7e8ff44d6618bbeeafc4b779b11
flored27 <dflores@fandm.edu>
2017-10-26 23:37:00 -0400

    second edit

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,3 +1,4 @@
+require 'terminal-table'
 class Coach < ActiveRecord::Base
   has_one :team
   before_create :to_head_coach
@@ -6,6 +7,18 @@ class Coach < ActiveRecord::Base
   def to_head_coach
     self.head_coach = "#{self.first_name} #{self.last_name}"
   end
+
+
+
+  def self.coach_table(coach_n)
+
+  table = Terminal::Table.new
+  table.title = "{coach_n}"
+  table.headings = ['Rank', 'Years Active', 'SB Rings']
+  table.rows = [["{self.rank}"], ["{self.years_active}"], ["{self.superbowl_titles}"]]
+  table.style = {:padding_left => 3, :border_x => "=", :border_i => "x"}
+  puts table
+end
 
   # def to_head_coach
   #   split_name = self.full_name.split(/[, ]+/)
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ Bundler.require
 #Bundler.require
 require_all('app')
 
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => 'db/nfl_players.db'
